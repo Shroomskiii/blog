a06eb7162b7fef15c6e76e7ccdd484b1c5dd57b8
Shroomskiii <bloodfeniks@gmail.com>
2022-04-29 17:06:20 +0300

    another hot fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,18 @@
 class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = Post.find(params[:post_id]).comments.new(comment_params)
+    @comment.assign_attributes(user_id: current_user.id)
+
     if !@comment.save
-      flash[:notice] = @commeny.errors.full_messages.to_sentence
+      flash[:notice] = @comment.errors.full_messages.to_sentence
     end
     redirect_to post_path(params[:post_id])
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(post_id: params[:post_id])
+    params.require(:comment).permit(:content, :post_id)
   end
 
 end
